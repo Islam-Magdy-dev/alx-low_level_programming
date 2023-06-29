3f3fa5bbd175707aac1bf23308634288c043f0e3
simz92 <magdy.designs@hotmail.com>
2023-06-29 16:49:13 -0700

    message

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -4,7 +4,6 @@ int main(void)
 int n;
 int a[5];
 int *p;
-
 a[2] = 1024;
 p = &n;
 /*
@@ -17,6 +16,6 @@ p = &n;
 */
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
-printf("a[2] = %d\n", a[2]);
+printf("a[2] = %p\n", a[2]);
 return (0);
 }
