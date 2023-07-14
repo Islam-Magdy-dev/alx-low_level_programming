4a2d1c5ea810d16d3e159e66d17ab30207d37de5
simz92 <magdy.designs@hotmail.com>
2023-07-13 18:12:34 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -1,21 +1,18 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - check the code for ALX School students.
+ * main - check the code
  *
  * Return: Always 0.
-*/
-
+ */
 int main(void)
 {
-	char *concat, *concat2;
+	char *concat;
 
-	concat = string_nconcat("Holberton ", "School !!!", 6);
-	concat2 = string_nconcat("Hello", NULL, 12); string_nconcat(NULL, "Hello", 0); string_nconcat(NULL, NULL, 10);
+	concat = string_nconcat("Best ", "School !!!", 6);
 	printf("%s\n", concat);
-	printf("%s\n: ", concat2);
 	free(concat);
 	return (0);
 }
