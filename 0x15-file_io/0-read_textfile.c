284ebd00b61b5acbc4487f94a40d1fa06a23934b
Islam Magdy <128490175+Islam-Magdy-dev@users.noreply.github.com>
2023-09-05 23:41:22 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,36 +7,43 @@
  * @letters: Input Number
  *
  * Return: Depend Condition
-*/
-
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int f_op, f_rd, f_wr;
-	char *buffer;
+    int f_op, f_rd, f_wr;
+    char *buffer;
 
-	if (filename == NULL)
-		return (0);
+    if (filename == NULL)
+        return (0);
 
-	f_op = open(filename, O_RDONLY);
-	if (f_op < 0)
-		return (0);
-	buffer = (char *) malloc(letters * sizeof(char));
-	if (buffer == NULL)
-		return (0);
-	f_rd = read(f_op, buffer, letters);
-	if (f_rd < 0)
-	{
-		free(buffer);
-		return (0);
-	}
-	buffer[f_rd] = '\0';
-	f_wr = write(STDOUT_FILENO, buffer, f_rd);
-	if (f_wr < 0)
-	{
-		free(buffer);
-		return (0);
-	}
-	free(buffer);
-	close(f_op);
-	return (f_wr);
+    f_op = open(filename, O_RDONLY);
+    if (f_op == -1)
+        return (0);
+
+    buffer = (char *)malloc(letters * sizeof(char));
+    if (buffer == NULL)
+    {
+        close(f_op);
+        return (0);
+    }
+
+    f_rd = read(f_op, buffer, letters);
+    if (f_rd == -1)
+    {
+        free(buffer);
+        close(f_op);
+        return (0);
+    }
+
+    f_wr = write(STDOUT_FILENO, buffer, f_rd);
+    if (f_wr == -1 || f_rd != f_wr)
+    {
+        free(buffer);
+        close(f_op);
+        return (0);
+    }
+
+    free(buffer);
+    close(f_op);
+    return (f_wr);
 }
