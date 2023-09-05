2b93d3bb5ec359886f1a34472a3d7f3c1d69b395
Islam Magdy <128490175+Islam-Magdy-dev@users.noreply.github.com>
2023-09-06 01:36:24 +0300

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -44,7 +44,7 @@ void check_elf(unsigned char *e_ident)
 * print_magic - Prints the magic numbers of an ELF header.
 * @e_ident: A pointer to an array containing the ELF magic numbers.
 *
-* Description: Magic numbers are separated by spaces.
+* Description: Spaces separate magic numbers.
 */
 void print_magic(unsigned char *e_ident)
 {
@@ -258,6 +258,44 @@ void close_elf(int elf)
 		exit(98);
 	}
 }
+
+/**
+* main - Displays the information contained in the
+*        ELF header at the start of an ELF file.
+* @argc: The number of arguments supplied to the program.
+* @argv: An array of pointers to the arguments.
+*
+* Return: 0 on success.
+*
+* Description: If the file is not an ELF File or
+*              the function fails - exit code 98.
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	Elf64_Ehdr *header;
+	int o, r;
+
+	o = open(argv[1], O_RDONLY);
+	if (o == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	header = malloc(sizeof(Elf64_Ehdr));
+	if (header == NULL)
+	{
+		close_elf(o);
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	r = read(o, header, sizeof(Elf64_Ehdr));
+	if (r == -1)
+	{
+		free(header);
+		close_elf(o);
+		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
+		exit(98);
+	}
 
 	check_elf(header->e_ident);
 	printf("ELF Header:\n");
